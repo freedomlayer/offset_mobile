206d057acf11a2caf1bb52d3132e27894612ae4e
real <real@freedomlayer.org>
2020-02-12 18:17:43 +0200

    state: Initial work on BuyView. Not done yet

--- lib/state/views/buy.dart
+++ lib/state/views/buy.dart
@@ -3,13 +3,38 @@ import 'package:built_value/serializer.dart';
 import 'package:built_union/built_union.dart';
 import 'package:meta/meta.dart';
 
+import '../../protocol/common.dart';
+
 part 'buy.g.dart';
 
 @BuiltUnion()
 class BuyView extends _$BuyView {
   static Serializer<BuyView> get serializer => _$buyViewSerializer;
 
-  // TODO: Fill in here:
-  BuyView.variant(int myInt) : super.variant(myInt);
+  BuyView.selectCard() : super.selectCard();
+  BuyView.cardBuy(CardBuyView cardBuy) : super.cardBuy(cardBuy);
 }
+
+abstract class CardBuyView implements Built<CardBuyView, CardBuyViewBuilder> {
+  static Serializer<CardBuyView> get serializer => _$cardBuyViewSerializer;
+
+  NodeName get nodeName;
+  CardBuyViewInner get inner;
+
+  CardBuyView._();
+  factory CardBuyView([void Function(CardBuyViewBuilder) updates]) = _$CardBuyView;
+}
+
+
+@BuiltUnion()
+class CardBuyViewInner extends _$CardBuyViewInner {
+  static Serializer<CardBuyViewInner> get serializer => _$cardBuyViewInnerSerializer;
+
+  CardBuyViewInner.invoiceSelect() : super.invoiceSelect();
+  CardBuyViewInner.invoiceScan() : super.invoiceScan();
+  // TODO: Add here:
+  // CardBuyViewInner.invoiceFile() : super.invoiceFile();
+  // CardBuyViewInner.invoiceFile() : super.invoiceFile();
+}
+
 
