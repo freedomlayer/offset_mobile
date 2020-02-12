251b79517c9146dd3bf46aa4821801e36eee644e
real <real@freedomlayer.org>
2020-02-12 17:34:40 +0200

    state: OutTransactionsView

--- lib/state/views/out_transactions.dart
+++ lib/state/views/out_transactions.dart
@@ -3,13 +3,15 @@ import 'package:built_value/serializer.dart';
 import 'package:built_union/built_union.dart';
 import 'package:meta/meta.dart';
 
+import '../../protocol/common.dart';
+
 part 'out_transactions.g.dart';
 
 @BuiltUnion()
 class OutTransactionsView extends _$OutTransactionsView {
   static Serializer<OutTransactionsView> get serializer => _$outTransactionsViewSerializer;
 
-  // TODO: Fill in here:
-  OutTransactionsView.variant(int myInt) : super.variant(myInt);
+  OutTransactionsView.main() : super.main();
+  OutTransactionsView.transaction(PaymentId paymentId) : super.transaction(paymentId);
 }
 
