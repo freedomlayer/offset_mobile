4a96b16e4da1b1e5ade5cc458fae9c08e4e59bec
real <real@freedomlayer.org>
2020-02-13 16:47:34 +0200

    protocol: Added FriendFile

--- lib/protocol/file.dart
+++ lib/protocol/file.dart
@@ -1,10 +1,11 @@
-// import 'package:built_collection/built_collection.dart';
+import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
 // import 'package:built_union/built_union.dart';
 // import 'package:meta/meta.dart';
 
 import 'common.dart';
+import 'compact.dart';
 
 part 'file.g.dart';
 
@@ -19,6 +20,18 @@ abstract class InvoiceFile implements Built<InvoiceFile, InvoiceFileBuilder> {
 
   InvoiceFile._();
   factory InvoiceFile([void Function(InvoiceFileBuilder) updates]) = _$InvoiceFile;
+}
+
+
+abstract class FriendFile implements Built<FriendFile, FriendFileBuilder> {
+  static Serializer<FriendFile> get serializer => _$friendFileSerializer;
+
+  PublicKey get friendPublicKey;
+  BuiltList<RelayAddress> get relays;
+  String get name;
+
+  FriendFile._();
+  factory FriendFile([void Function(FriendFileBuilder) updates]) = _$FriendFile;
 }
 
 
