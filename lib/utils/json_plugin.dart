02265551385e685f97d01ddaa727d4a66e3431d9
real <real@freedomlayer.org>
2020-02-25 17:19:53 +0200

    utils: json_plugin: Fixed BuiltMap non string keys serialization

--- lib/utils/json_plugin.dart
+++ lib/utils/json_plugin.dart
@@ -1,3 +1,4 @@
+import 'package:built_collection/built_collection.dart';
 import 'package:built_value/json_object.dart';
 import 'package:built_union/custom_json_plugin.dart';
 import 'package:built_value/serializer.dart';
@@ -9,7 +10,9 @@ class CommJsonPlugin extends CustomJsonPlugin {
     var i = 0;
     map.forEach((key, value) {
       // Drop null values, they are represented by missing keys.
-      if (value == null) return;
+      if (value == null) {
+        return;
+      }
 
       result[i] = key;
       result[i + 1] = value;
@@ -28,6 +31,34 @@ class CommJsonPlugin extends CustomJsonPlugin {
       }
     } else {
       return super.beforeDeserialize(object, specifiedType);
+    }
+  }
+
+  /// Converts serialization output, a `List`, to a `Map`, when the serialized
+  /// type is known statically.
+  Map _toMap(List list) {
+    var result = <String, Object>{};
+    for (var i = 0; i != list.length ~/ 2; ++i) {
+      final key = list[i * 2];
+      final value = list[i * 2 + 1];
+      result[key as String] = value;
+    }
+    return result;
+  }
+
+  // Fix special case of BuiltMap keys. Avoids double encoding of key ("myKey"
+  // instead of ""myKey"").
+  @override
+  Object afterSerialize(Object object, FullType specifiedType) {
+    if (object is List &&
+        specifiedType.root == BuiltMap) {
+      if (specifiedType.isUnspecified) {
+        return super.afterSerialize(object, specifiedType);
+      } else {
+        return _toMap(object);
+      }
+    } else {
+      return super.afterSerialize(object, specifiedType);
     }
   }
 }
