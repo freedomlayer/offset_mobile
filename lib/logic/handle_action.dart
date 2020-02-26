b98394c3ad897a73588784e0aa720198b6cf0325
real <real@freedomlayer.org>
2020-02-26 13:30:12 +0200

    logic: Some work on handleAction()

--- lib/logic/handle_action.dart
+++ lib/logic/handle_action.dart
@@ -2,10 +2,38 @@ import '../actions/actions.dart';
 // import '../protocol/protocol.dart';
 import '../state/state.dart';
 
-// import 'types.dart';
-// import 'handle_shared_file.dart';
-// import 'handle_action.dart';
-
 AppState handleAction(AppState appState, AppAction appAction) {
+  return appAction.match(
+      home: (homeAction) => handleHomeAction(appState, homeAction),
+      buy: (buyAction) => handleBuyAction(appState, buyAction),
+      sell: (sellAction) => handleSellAction(appState, sellAction),
+      outTransactions: (outTransactionsAction) => handleOutTransactionsAction(appState, outTransactionsAction),
+      balances: (balancesAction) => handleBalancesAction(appState, balancesAction),
+      settings: (settingsAction) => handleSettingsAction(appState, settingsAction));
+}
+
+
+AppState handleHomeAction(AppState appState, HomeAction homeAction) {
   throw UnimplementedError();
 }
+
+AppState handleBuyAction(AppState appState, BuyAction buyAction) {
+  throw UnimplementedError();
+}
+
+AppState handleSellAction(AppState appState, SellAction sellAction) {
+  throw UnimplementedError();
+}
+
+AppState handleOutTransactionsAction(AppState appState, OutTransactionsAction outTransactionsAction) {
+  throw UnimplementedError();
+}
+
+AppState handleBalancesAction(AppState appState, BalancesAction balancesAction) {
+  throw UnimplementedError();
+}
+
+AppState handleSettingsAction(AppState appState, SettingsAction settingsAction) {
+  throw UnimplementedError();
+}
+
