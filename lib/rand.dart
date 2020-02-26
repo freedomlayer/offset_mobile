a0dea0b4be202122cf4c75052e432a0945f8b9f4
real <real@freedomlayer.org>
2020-02-26 16:48:04 +0200

    rand: Initial implementation

---
+++ lib/rand.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'dart:math';
+import 'protocol/protocol.dart';
+
+const int UID_LENGTH = 16;
+
+// final Random _random = Random.secure();
+
+/// Return `length` random bytes, encoded in base64
+String _randBytesB64(Random rand, int length) {
+  final values = List<int>.generate(length, (i) => rand.nextInt(0x100));
+  return base64Url.encode(values);
+}
+
+
+/// Generate a random Uid
+Uid genUid(Random rand) {
+  return Uid(_randBytesB64(rand, UID_LENGTH));
+}
+
+
