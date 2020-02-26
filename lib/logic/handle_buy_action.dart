4dc000fb3c20c39914c7f4ec7cf4c07177c5c5f1
real <real@freedomlayer.org>
2020-02-26 16:47:47 +0200

    logic: Some work on handle_buy_action.dart

--- lib/logic/handle_buy_action.dart
+++ lib/logic/handle_buy_action.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+
 import 'package:built_collection/built_collection.dart';
 
 import '../actions/actions.dart';
@@ -14,7 +16,41 @@ AppState handleBuyAction(BuyView buyView,
   return buyAction.match(
       back: () => createState(AppView.home()),
       loadInvoice: (invoiceFile) => createState(AppView.buy(BuyView.invoiceInfo(invoiceFile))),
-      confirmInvoice: () => throw UnimplementedError(),
-      selectCard: (nodeName) => throw UnimplementedError(),
+      confirmInvoice: () {
+        final invoiceFile = buyView.match(
+            invoiceSelect: () => null,
+            invoiceInfo: (invoiceFile) => invoiceFile,
+            selectCard: (_) => null,
+            paymentProgress: (_a, _b) => null);
+
+        if (invoiceFile != null) {
+          return createState(AppView.buy(BuyView.selectCard(invoiceFile)));
+        } else {
+          developer.log('handleBuyAction(): Received confirmInvoice action during incorrect view');
+          return createState(AppView.buy(buyView));
+        }
+      },
+      selectCard: (nodeName) {
+        final invoiceFile = buyView.match(
+            invoiceSelect: () => null,
+            invoiceInfo: (_) => null,
+            selectCard: (invoiceFile) => invoiceFile,
+            paymentProgress: (_a, _b) => null);
+
+        if (invoiceFile != null) {
+          /*
+          final requestId = genRequestId();
+          final serverToUser = ServerToUser.node(nodeId, compactToUser);
+          final userToServerAck = UserToServerAck((b) => b..requestId = requetId
+                                                       ..inner = serverToUser);
+          return createState(AppView.buy(BuyView.paymentProgress(nodeName, paymentId)));
+          */
+          throw UnimplementedError();
+        } else {
+          developer.log('handleBuyAction(): Received selectCard action during incorrect view');
+          return createState(AppView.buy(buyView));
+        }
+
+      },
       cancelPayment: () => throw UnimplementedError());
 }
