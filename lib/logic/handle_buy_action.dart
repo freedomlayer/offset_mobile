b433952bab37a9bf4b81da149f1abc88c00a3531
real <real@freedomlayer.org>
2020-02-26 17:31:09 +0200

    logic: handle_buy_action: Implemented cancelPayment action

--- lib/logic/handle_buy_action.dart
+++ lib/logic/handle_buy_action.dart
@@ -66,6 +66,7 @@ AppState handleBuyAction(
           return createState(AppView.buy(buyView));
         }
 
+        // Send an InitPayment message:
         final requestId = genUid(rand);
         final paymentId = genPaymentId(rand);
         final initPayment = InitPayment((b) => b
@@ -94,6 +95,54 @@ AppState handleBuyAction(
               oldView, newView, nextRequests, optPendingRequest));
       },
       cancelPayment: () {
-        throw UnimplementedError();
+
+        PaymentId paymentId;
+        NodeName nodeName;
+
+        buyView.match(
+            invoiceSelect: () => null,
+            invoiceInfo: (_) => null,
+            selectCard: (_) => null,
+            paymentProgress: (nodeName0, paymentId0) {
+              nodeName = nodeName0;
+              paymentId = paymentId0;
+              return null;
+            });
+
+        if (nodeName == null) {
+          developer.log(
+              'handleBuyAction(): Received cancelPayment action during incorrect view');
+          return createState(AppView.buy(buyView));
+
+        }
+
+        final nodeId = nodeIdByNodeName(nodeName, nodesStates);
+        if (nodeId == null) {
+          developer
+              .log('handleBuyAction(): cancelPayment: node $nodeName is not open');
+          return createState(AppView.home());
+        }
+
+        // At this point paymentId and nodeName should be non null.
+
+        // Send a CancelPayment message:
+        final userToCompact = UserToCompact.cancelPayment(paymentId);
+        final userToServer = UserToServer.node(nodeId, userToCompact);
+        final requestId = genUid(rand);
+        final userToServerAck = UserToServerAck((b) => b
+          ..requestId = requestId
+          ..inner = userToServer);
+
+        final oldView = AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
+        final newView = AppView.home();
+
+        final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
+        final optPendingRequest = OptPendingRequest.none();
+
+        return AppState((b) => b
+          ..nodesStates = nodesStates.toBuilder()
+          ..viewState = ViewState.transition(
+              oldView, newView, nextRequests, optPendingRequest));
+
       });
 }
