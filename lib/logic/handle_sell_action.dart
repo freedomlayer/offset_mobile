95377811a957666e8ea9df010cc7e608793c03c8
real <real@freedomlayer.org>
2020-02-26 19:08:19 +0200

    logic: Initial work on handle_sell_action

--- lib/logic/handle_sell_action.dart
+++ lib/logic/handle_sell_action.dart
@@ -6,6 +6,16 @@ import '../state/state.dart';
 
 AppState handleSellAction(SellView sellView,
     BuiltMap<NodeName, NodeState> nodesStates, SellAction sellAction) {
-  throw UnimplementedError();
+
+  final createState = (AppView appView) => AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.view(appView));
+
+  return sellAction.match(
+      back: () => createState(AppView.home()),
+      selectCard: (nodeName) => createState(AppView.sell(SellView.invoiceDetails(nodeName))),
+      createInvoice: (currency, amount, description) => throw UnimplementedError(),
+      viewTransaction: () => throw UnimplementedError(),
+      cancelInvoice: (invoiceId) => throw UnimplementedError());
 }
 
