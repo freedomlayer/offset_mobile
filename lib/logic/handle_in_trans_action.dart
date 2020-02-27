bdbe630f83580310f2c4148a511a114d9739e5c2
real <real@freedomlayer.org>
2020-02-27 16:19:16 +0200

    logic: Some work on handle_in_trans_action. Not done yet

--- lib/logic/handle_in_trans_action.dart
+++ lib/logic/handle_in_trans_action.dart
@@ -1,3 +1,5 @@
+import 'dart:developer' as developer;
+
 import 'package:built_collection/built_collection.dart';
 
 import '../actions/actions.dart';
@@ -8,5 +10,39 @@ AppState handleInTransactionsAction(
     InTransactionsView inTransactionsView,
     BuiltMap<NodeName, NodeState> nodesStates,
     InTransactionsAction inTransactionsAction) {
-  throw UnimplementedError();
+
+  final createState = (AppView appView) => AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.view(appView));
+
+  return inTransactionsAction.match(
+      back: () {
+        return inTransactionsView.match(
+            home: () => createState(AppView.home()),
+            transaction: (_a, _b) => createState(AppView.inTransactions(InTransactionsView.home())),
+            sendInvoice: (nodeName, invoiceId) => createState(AppView.inTransactions(InTransactionsView.transaction(nodeName, invoiceId))),
+            collected: (nodeName, invoiceId) => createState(AppView.inTransactions(InTransactionsView.home())),
+            selectCardApplyCommit: (_) => createState(AppView.home()));
+      },
+      selectInvoice: (nodeName, invoiceId) => createState(AppView.inTransactions(InTransactionsView.transaction(nodeName, invoiceId))),
+      applyCommit: (commit) => throw UnimplementedError(),
+      resendInvoice: () {
+        final newAppState = inTransactionsView.match(
+            home: () => null,
+            transaction: (nodeName, invoiceId) => createState(AppView.inTransactions(InTransactionsView.sendInvoice(nodeName, invoiceId))),
+            sendInvoice: (_nodeName, _invoiceId) => null,
+            collected: (_nodeName, _invoiceId) => null,
+            selectCardApplyCommit: (_) => null);
+
+        if (newAppState == null) {
+          developer.log(
+              'handleInTransactionsAction(): Received action resendInvoice during wrong view');
+
+          return createState(AppView.inTransactions(inTransactionsView));
+        } else {
+          return newAppState;
+        }
+      },
+      collectInvoice: () => throw UnimplementedError(),
+      cancelInvoice: () => throw UnimplementedError());
 }
