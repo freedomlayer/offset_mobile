a2a28ed885f9cffd3825908b9ece60ff90b6b8db
real <real@freedomlayer.org>
2020-02-27 15:33:57 +0200

    logic: handle_buy_action: Refactoring

--- lib/logic/handle_buy_action.dart
+++ lib/logic/handle_buy_action.dart
@@ -46,195 +46,223 @@ AppState handleBuyAction(
           return createState(AppView.buy(buyView));
         }
       },
-      selectCard: (nodeName) {
-        final nodeId = nodeIdByNodeName(nodeName, nodesStates);
-        if (nodeId == null) {
-          developer
-              .log('handleBuyAction(): selectCard: node $nodeName is not open');
-          return createState(AppView.home());
-        }
+      selectCard: (nodeName) => handleBuySelectCardAction(
+          nodeName, buyView, nodesStates, buyAction, rand),
+      confirmFees: () =>
+          handleBuyConfirmFeesAction(buyView, nodesStates, buyAction, rand),
+      cancelPayment: () =>
+          handleBuyCancelPaymentAction(buyView, nodesStates, buyAction, rand));
+}
 
-        final invoiceFile = buyView.match(
-            invoiceSelect: () => null,
-            invoiceInfo: (_) => null,
-            selectCard: (invoiceFile) => invoiceFile,
-            paymentProgress: (_a, _b) => null);
+AppState handleBuySelectCardAction(
+    NodeName nodeName,
+    BuyView buyView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    BuyAction buyAction,
+    Random rand) {
+  final createState = (AppView appView) => AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.view(appView));
 
-        if (invoiceFile == null) {
-          developer.log(
-              'handleBuyAction(): Received selectCard action during incorrect view');
-          return createState(AppView.buy(buyView));
-        }
+  final nodeId = nodeIdByNodeName(nodeName, nodesStates);
+  if (nodeId == null) {
+    developer.log('handleBuyAction(): selectCard: node $nodeName is not open');
+    return createState(AppView.home());
+  }
 
-        // Send an InitPayment message:
-        final requestId = genUid(rand);
-        final paymentId = genPaymentId(rand);
-        final initPayment = InitPayment((b) => b
-          ..paymentId = paymentId
-          ..invoiceId = invoiceFile.invoiceId
-          ..currency = invoiceFile.currency
-          ..destPublicKey = invoiceFile.destPublicKey
-          ..destPayment = invoiceFile.destPayment
-          ..description = invoiceFile.description);
-        final userToCompact = UserToCompact.initPayment(initPayment);
-        final userToServer = UserToServer.node(nodeId, userToCompact);
-        final userToServerAck = UserToServerAck((b) => b
-          ..requestId = requestId
-          ..inner = userToServer);
+  final invoiceFile = buyView.match(
+      invoiceSelect: () => null,
+      invoiceInfo: (_) => null,
+      selectCard: (invoiceFile) => invoiceFile,
+      paymentProgress: (_a, _b) => null);
 
-        final oldView = AppView.buy(BuyView.selectCard(invoiceFile));
-        final newView =
-            AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
+  if (invoiceFile == null) {
+    developer.log(
+        'handleBuyAction(): Received selectCard action during incorrect view');
+    return createState(AppView.buy(buyView));
+  }
 
-        final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
-        final optPendingRequest = OptPendingRequest.none();
+  // Send an InitPayment message:
+  final requestId = genUid(rand);
+  final paymentId = genPaymentId(rand);
+  final initPayment = InitPayment((b) => b
+    ..paymentId = paymentId
+    ..invoiceId = invoiceFile.invoiceId
+    ..currency = invoiceFile.currency
+    ..destPublicKey = invoiceFile.destPublicKey
+    ..destPayment = invoiceFile.destPayment
+    ..description = invoiceFile.description);
+  final userToCompact = UserToCompact.initPayment(initPayment);
+  final userToServer = UserToServer.node(nodeId, userToCompact);
+  final userToServerAck = UserToServerAck((b) => b
+    ..requestId = requestId
+    ..inner = userToServer);
 
-        return AppState((b) => b
-          ..nodesStates = nodesStates.toBuilder()
-          ..viewState = ViewState.transition(
-              oldView, newView, nextRequests, optPendingRequest));
-      },
-      confirmFees: () {
-        PaymentId paymentId;
-        NodeName nodeName;
+  final oldView = AppView.buy(BuyView.selectCard(invoiceFile));
+  final newView = AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
 
-        buyView.match(
-            invoiceSelect: () => null,
-            invoiceInfo: (_) => null,
-            selectCard: (_) => null,
-            paymentProgress: (nodeName0, paymentId0) {
-              nodeName = nodeName0;
-              paymentId = paymentId0;
-              return null;
-            });
+  final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
+  final optPendingRequest = OptPendingRequest.none();
 
-        if (nodeName == null) {
-          developer.log(
-              'handleBuyAction(): Received confirmFees action during incorrect view');
-          return createState(AppView.buy(buyView));
-        }
+  return AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.transition(
+        oldView, newView, nextRequests, optPendingRequest));
+}
 
-        // At this point paymentId and nodeName should be non null.
+AppState handleBuyConfirmFeesAction(
+    BuyView buyView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    BuyAction buyAction,
+    Random rand) {
+  final createState = (AppView appView) => AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.view(appView));
 
-        // Send a ConfirmPaymentFees message:
+  PaymentId paymentId;
+  NodeName nodeName;
 
-        // nodeState is not null because we managed to get nodeId earlier
-        NodeId nodeId;
-        Uid confirmId;
-
-        final nodeState = nodesStates[nodeName];
-        if (nodeState == null) {
-          developer.log(
-              'handleBuyAction(): confirmFees: node $nodeName does not exist!');
-          return createState(AppView.home());
-        }
-
-        final newState = nodeState.inner.match(closed: () {
-          developer
-              .log('handleBuyAction(): confirmFees: node $nodeName is closed!');
-          return createState(AppView.home());
-        }, open: (nodeOpen) {
-          final openPayment = nodeOpen.compactReport.openPayments[paymentId];
-          if (openPayment == null) {
-            developer.log(
-                'handleBuyAction(): confirmFees: payment $paymentId does not exist!');
-            return createState(AppView.home());
-          }
-
-          openPayment.status.match(
-              searchingRoute: (_) => null,
-              foundRoute: (confirmId0, _fees) {
-                confirmId = confirmId0;
-                return null;
-              },
-              sending: (_) => null,
-              commit: (_a, _b) => null,
-              success: (_a, _b, _c) => null,
-              failure: (_) => null);
-
-          if (confirmId == null) {
-            developer.log(
-                'handleBuyAction(): confirmFees: payment $paymentId is not waiting for confirmation!');
-            return createState(AppView.buy(buyView));
-          }
-          return null;
-        });
-
-        // If any error occured, we return early:
-        if (newState != null) {
-          return newState;
-        }
-
-        final confirmPaymentFees = ConfirmPaymentFees((b) => b
-          ..paymentId = paymentId
-          ..confirmId = confirmId);
-        final userToCompact =
-            UserToCompact.confirmPaymentFees(confirmPaymentFees);
-        final userToServer = UserToServer.node(nodeId, userToCompact);
-        final requestId = genUid(rand);
-        final userToServerAck = UserToServerAck((b) => b
-          ..requestId = requestId
-          ..inner = userToServer);
-
-        final oldView =
-            AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
-        final newView = AppView.home();
-
-        final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
-        final optPendingRequest = OptPendingRequest.none();
-
-        return AppState((b) => b
-          ..nodesStates = nodesStates.toBuilder()
-          ..viewState = ViewState.transition(
-              oldView, newView, nextRequests, optPendingRequest));
-      },
-      cancelPayment: () {
-        PaymentId paymentId;
-        NodeName nodeName;
-
-        buyView.match(
-            invoiceSelect: () => null,
-            invoiceInfo: (_) => null,
-            selectCard: (_) => null,
-            paymentProgress: (nodeName0, paymentId0) {
-              nodeName = nodeName0;
-              paymentId = paymentId0;
-              return null;
-            });
-
-        if (nodeName == null) {
-          developer.log(
-              'handleBuyAction(): Received cancelPayment action during incorrect view');
-          return createState(AppView.buy(buyView));
-        }
-
-        final nodeId = nodeIdByNodeName(nodeName, nodesStates);
-        if (nodeId == null) {
-          developer.log(
-              'handleBuyAction(): cancelPayment: node $nodeName is not open');
-          return createState(AppView.home());
-        }
-
-        // At this point paymentId and nodeName should be non null.
-
-        // Send a CancelPayment message:
-        final userToCompact = UserToCompact.cancelPayment(paymentId);
-        final userToServer = UserToServer.node(nodeId, userToCompact);
-        final requestId = genUid(rand);
-        final userToServerAck = UserToServerAck((b) => b
-          ..requestId = requestId
-          ..inner = userToServer);
-
-        final oldView =
-            AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
-        final newView = AppView.home();
-
-        final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
-        final optPendingRequest = OptPendingRequest.none();
-
-        return AppState((b) => b
-          ..nodesStates = nodesStates.toBuilder()
-          ..viewState = ViewState.transition(
-              oldView, newView, nextRequests, optPendingRequest));
+  buyView.match(
+      invoiceSelect: () => null,
+      invoiceInfo: (_) => null,
+      selectCard: (_) => null,
+      paymentProgress: (nodeName0, paymentId0) {
+        nodeName = nodeName0;
+        paymentId = paymentId0;
+        return null;
       });
+
+  if (nodeName == null) {
+    developer.log(
+        'handleBuyAction(): Received confirmFees action during incorrect view');
+    return createState(AppView.buy(buyView));
+  }
+
+  // At this point paymentId and nodeName should be non null.
+
+  // Send a ConfirmPaymentFees message:
+
+  // nodeState is not null because we managed to get nodeId earlier
+  NodeId nodeId;
+  Uid confirmId;
+
+  final nodeState = nodesStates[nodeName];
+  if (nodeState == null) {
+    developer
+        .log('handleBuyAction(): confirmFees: node $nodeName does not exist!');
+    return createState(AppView.home());
+  }
+
+  final newState = nodeState.inner.match(closed: () {
+    developer.log('handleBuyAction(): confirmFees: node $nodeName is closed!');
+    return createState(AppView.home());
+  }, open: (nodeOpen) {
+    final openPayment = nodeOpen.compactReport.openPayments[paymentId];
+    if (openPayment == null) {
+      developer.log(
+          'handleBuyAction(): confirmFees: payment $paymentId does not exist!');
+      return createState(AppView.home());
+    }
+
+    openPayment.status.match(
+        searchingRoute: (_) => null,
+        foundRoute: (confirmId0, _fees) {
+          confirmId = confirmId0;
+          return null;
+        },
+        sending: (_) => null,
+        commit: (_a, _b) => null,
+        success: (_a, _b, _c) => null,
+        failure: (_) => null);
+
+    if (confirmId == null) {
+      developer.log(
+          'handleBuyAction(): confirmFees: payment $paymentId is not waiting for confirmation!');
+      return createState(AppView.buy(buyView));
+    }
+    return null;
+  });
+
+  // If any error occured, we return early:
+  if (newState != null) {
+    return newState;
+  }
+
+  final confirmPaymentFees = ConfirmPaymentFees((b) => b
+    ..paymentId = paymentId
+    ..confirmId = confirmId);
+  final userToCompact = UserToCompact.confirmPaymentFees(confirmPaymentFees);
+  final userToServer = UserToServer.node(nodeId, userToCompact);
+  final requestId = genUid(rand);
+  final userToServerAck = UserToServerAck((b) => b
+    ..requestId = requestId
+    ..inner = userToServer);
+
+  final oldView = AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
+  final newView = AppView.home();
+
+  final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
+  final optPendingRequest = OptPendingRequest.none();
+
+  return AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.transition(
+        oldView, newView, nextRequests, optPendingRequest));
+}
+
+AppState handleBuyCancelPaymentAction(
+    BuyView buyView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    BuyAction buyAction,
+    Random rand) {
+  final createState = (AppView appView) => AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.view(appView));
+
+  PaymentId paymentId;
+  NodeName nodeName;
+
+  buyView.match(
+      invoiceSelect: () => null,
+      invoiceInfo: (_) => null,
+      selectCard: (_) => null,
+      paymentProgress: (nodeName0, paymentId0) {
+        nodeName = nodeName0;
+        paymentId = paymentId0;
+        return null;
+      });
+
+  if (nodeName == null) {
+    developer.log(
+        'handleBuyAction(): Received cancelPayment action during incorrect view');
+    return createState(AppView.buy(buyView));
+  }
+
+  final nodeId = nodeIdByNodeName(nodeName, nodesStates);
+  if (nodeId == null) {
+    developer
+        .log('handleBuyAction(): cancelPayment: node $nodeName is not open');
+    return createState(AppView.home());
+  }
+
+  // At this point paymentId and nodeName should be non null.
+
+  // Send a CancelPayment message:
+  final userToCompact = UserToCompact.cancelPayment(paymentId);
+  final userToServer = UserToServer.node(nodeId, userToCompact);
+  final requestId = genUid(rand);
+  final userToServerAck = UserToServerAck((b) => b
+    ..requestId = requestId
+    ..inner = userToServer);
+
+  final oldView = AppView.buy(BuyView.paymentProgress(nodeName, paymentId));
+  final newView = AppView.home();
+
+  final nextRequests = BuiltList<UserToServerAck>([userToServerAck]);
+  final optPendingRequest = OptPendingRequest.none();
+
+  return AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.transition(
+        oldView, newView, nextRequests, optPendingRequest));
 }
