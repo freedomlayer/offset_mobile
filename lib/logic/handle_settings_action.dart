ea003e7c1a97a497e860134ec1b5e4939d286b23
real <real@freedomlayer.org>
2020-03-01 13:15:50 +0200

    logic: Some work on handleSettingsAction()

--- lib/logic/handle_settings_action.dart
+++ lib/logic/handle_settings_action.dart
@@ -1,24 +1,68 @@
+import 'dart:developer' as developer;
 import 'package:built_collection/built_collection.dart';
 
 import '../actions/actions.dart';
 import '../protocol/protocol.dart';
 import '../state/state.dart';
 
-
 AppState handleSettingsAction(SettingsView settingsView,
     BuiltMap<NodeName, NodeState> nodesStates, SettingsAction settingsAction) {
-
   final createState = (AppView appView) => AppState((b) => b
     ..nodesStates = nodesStates.toBuilder()
     ..viewState = ViewState.view(appView));
 
   return settingsAction.match(
       back: () => createState(AppView.home()),
-      newCard: (_newCardAction) => throw UnimplementedError(),
-      cardSettings: (_cardSettingsAction) => throw UnimplementedError(),
+      newCard: (newCardAction) {
+        final newCardView = settingsView.match(
+            home: () => null,
+            cardSettings: (_) => null,
+            newCard: (newCardView) => newCardView,
+            selectCardAddRelay: (_) => null,
+            selectCardAddIndex: (_) => null,
+            selectCardAddFriend: (_) => null);
+
+        if (newCardView == null) {
+          developer.log('handleSettingsAction: newCard: Incorrect view');
+          return createState(AppView.settings(settingsView));
+        }
+
+        return _handleNewCard(newCardView, nodesStates, newCardAction);
+      },
+      cardSettings: (cardSettingsAction) {
+        final cardSettingsView = settingsView.match(
+            home: () => null,
+            cardSettings: (cardSettingsView) => cardSettingsView,
+            newCard: (_) => null,
+            selectCardAddRelay: (_) => null,
+            selectCardAddIndex: (_) => null,
+            selectCardAddFriend: (_) => null);
+
+        if (cardSettingsView == null) {
+          developer.log('handleSettingsAction: cardSettings: Incorrect view');
+          return createState(AppView.settings(settingsView));
+        }
+
+        return _handleCardSettings(
+            cardSettingsView, nodesStates, cardSettingsAction);
+      },
       selectCard: (nodeName) {
-        final cardSettingsView = CardSettingsView((b) => b..nodeName = nodeName
-                                                          ..inner = CardSettingsInnerView.home());
-        return createState(AppView.settings(SettingsView.cardSettings(cardSettingsView)));
+        final cardSettingsView = CardSettingsView((b) => b
+          ..nodeName = nodeName
+          ..inner = CardSettingsInnerView.home());
+        return createState(
+            AppView.settings(SettingsView.cardSettings(cardSettingsView)));
       });
+}
+
+AppState _handleNewCard(NewCardView newCardView,
+    BuiltMap<NodeName, NodeState> nodesStates, NewCardAction newCardAction) {
+  throw UnimplementedError();
+}
+
+AppState _handleCardSettings(
+    CardSettingsView cardSettingsView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    CardSettingsAction cardSettingsAction) {
+  throw UnimplementedError();
 }
