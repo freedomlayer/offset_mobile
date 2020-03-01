d272a46e5a9e8fb4c50e07cda3a864c27c273e58
real <real@freedomlayer.org>
2020-03-01 12:39:41 +0200

    logic: initial work on handleSettingsAction()

--- lib/logic/handle_settings_action.dart
+++ lib/logic/handle_settings_action.dart
@@ -7,5 +7,18 @@ import '../state/state.dart';
 
 AppState handleSettingsAction(SettingsView settingsView,
     BuiltMap<NodeName, NodeState> nodesStates, SettingsAction settingsAction) {
-  throw UnimplementedError();
+
+  final createState = (AppView appView) => AppState((b) => b
+    ..nodesStates = nodesStates.toBuilder()
+    ..viewState = ViewState.view(appView));
+
+  return settingsAction.match(
+      back: () => createState(AppView.home()),
+      newCard: (_newCardAction) => throw UnimplementedError(),
+      cardSettings: (_cardSettingsAction) => throw UnimplementedError(),
+      selectCard: (nodeName) {
+        final cardSettingsView = CardSettingsView((b) => b..nodeName = nodeName
+                                                          ..inner = CardSettingsInnerView.home());
+        return createState(AppView.settings(SettingsView.cardSettings(cardSettingsView)));
+      });
 }
