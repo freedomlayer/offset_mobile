12ba15863fe494ae3ca493d2821015759b487611
real <real@freedomlayer.org>
2020-03-01 18:20:42 +0200

    logic: Initial work on handleFriendsSettings()

--- lib/logic/settings/friends.dart
+++ lib/logic/settings/friends.dart
@@ -9,12 +9,39 @@ import '../../state/state.dart';
 
 // import '../../rand.dart';
 
-
 AppState handleFriendsSettings(
     NodeName nodeName,
     FriendsSettingsView friendsSettingsView,
     BuiltMap<NodeName, NodeState> nodesStates,
     FriendsSettingsAction friendsSettingsAction,
     Random rand) {
-  throw UnimplementedError();
+  final createStateInner = (CardSettingsInnerView cardSettingsInnerView) =>
+      AppState((b) => b
+        ..nodesStates = nodesStates.toBuilder()
+        ..viewState = ViewState.view(
+            AppView.settings(SettingsView.cardSettings(CardSettingsView((b) => b
+              ..nodeName = nodeName
+              ..inner = cardSettingsInnerView)))));
+
+  return friendsSettingsAction.match(
+      back: () => friendsSettingsView.match(
+          home: () => createStateInner(CardSettingsInnerView.home()),
+          friendSettings: (_) => createStateInner(
+              CardSettingsInnerView.friends(FriendsSettingsView.home())),
+          newFriend: (_) => createStateInner(
+              CardSettingsInnerView.friends(FriendsSettingsView.home())),
+          shareInfo: () => createStateInner(
+              CardSettingsInnerView.friends(FriendsSettingsView.home()))),
+      selectNewFriend: () => createStateInner(
+          CardSettingsInnerView.friends(
+              FriendsSettingsView.newFriend(NewFriendView.select()))),
+      newFriend: (newFriendAction) => throw UnimplementedError(),
+      selectFriend: (friendPublicKey) => createStateInner(
+          CardSettingsInnerView.friends(
+              FriendsSettingsView.friendSettings(FriendSettingsView((b) => b
+                ..friendPublicKey = friendPublicKey
+                ..inner = FriendSettingsInnerView.home())))),
+      friendSettings: (friendSettingsAction) => throw UnimplementedError(),
+      shareInfo: () => createStateInner(
+          CardSettingsInnerView.friends(FriendsSettingsView.shareInfo())));
 }
