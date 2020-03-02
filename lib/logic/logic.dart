8b1e158b6a5768022bbffab3a221c0b22fe10d85
real <real@freedomlayer.org>
2020-03-02 20:01:10 +0200

    logic: Exporting handle_server_to_user_ack.dart

--- lib/logic/logic.dart
+++ lib/logic/logic.dart
@@ -7,6 +7,8 @@ import 'handle_shared_file.dart';
 import 'handle_action.dart';
 import 'handle_server_to_user_ack.dart';
 
+export 'handle_server_to_user_ack.dart';
+
 AppState handleAppEvent(AppState appState, AppEvent appEvent, Random rand) {
   return appEvent.match(
     sharedFile: (filePath) => handleSharedFile(appState, filePath),
