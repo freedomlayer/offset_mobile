028da071f9787a556d1d34408fec798ed6126441
real <real@freedomlayer.org>
2020-03-06 15:53:34 +0200

    process: Updated invocation of stcompact

--- lib/process.dart
+++ lib/process.dart
@@ -4,17 +4,25 @@ import 'package:path/path.dart' as path;
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
 
-const String STCOMPACT_FILENAME = 'stcompact-i686';
+const String STCOMPACT_FILENAME = 'stcompact_i686';
+const String STORE_DIR = 'store';
 
+/// Path to stcompact's executable
 Future<String> _getBinaryPath() async {
   String dir = (await getApplicationDocumentsDirectory()).path;
   return path.join(dir, STCOMPACT_FILENAME);
 }
 
+/// Path to stcompact's store
+Future<String> _getStorePath() async {
+  String dir = (await getApplicationDocumentsDirectory()).path;
+  return path.join(dir, STORE_DIR);
+}
+
 Future<void> loadBinaries() async {
   // Obtain the binary file from assets:
   var bytes =
-      await rootBundle.load("external/$STCOMPACT_FILENAME");
+      await rootBundle.load(path.join('external', STCOMPACT_FILENAME));
 
   // Get the path we want the binary file to live in:
   final String binaryPath = await _getBinaryPath();
@@ -27,6 +35,6 @@ Future<void> loadBinaries() async {
 }
 
 Future<Process> openProcess() async {
-  return Process.start(await _getBinaryPath(), []);
+  return Process.start(await _getBinaryPath(), ['--store', await _getStorePath()]);
 }
 
