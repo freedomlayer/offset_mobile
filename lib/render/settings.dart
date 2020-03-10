b265cee685cf178b4cbde82516b0e123e8b3be66
real <real@freedomlayer.org>
2020-03-10 19:08:32 +0200

    render: settings: Some work. Not done yet

--- lib/render/settings.dart
+++ lib/render/settings.dart
@@ -40,7 +40,7 @@ Widget _renderHome(BuiltMap<NodeName, NodeState> nodesStates,
     // final nodeState = entry.value;
     final cardEntry = ListTile(
         key: Key(nodeName.inner),
-        title: Text('$nodeName'),
+        title: Text('${nodeName.inner}'),
         onTap: () => queueAction(SettingsAction.selectCard(nodeName)));
     children.add(cardEntry);
   }
@@ -60,10 +60,29 @@ Widget _renderHome(BuiltMap<NodeName, NodeState> nodesStates,
       floatingActionButton: newCardButton);
 }
 
-Widget _renderNewCard(NewCardView newCardView, BuiltMap<NodeName, NodeState> nodesStates,
+Widget _renderNewCard(
+    NewCardView newCardView,
+    BuiltMap<NodeName, NodeState> nodesStates,
     Function(NewCardAction) queueAction) {
+  return newCardView.match(
+      select: () => _renderNewCardSelect(nodesStates, queueAction),
+      newLocal: () => _renderNewCardLocal(nodesStates, queueAction),
+      newRemote: () => _renderNewCardRemote(nodesStates, queueAction),
+      newRemoteName: (remoteCardFile) =>
+          _renderNewRemoteName(remoteCardFile, nodesStates, queueAction));
+}
 
-  final body = Column(children: []);
+Widget _renderNewCardSelect(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(NewCardAction) queueAction) {
+  final body = Center(
+      child: Column(children: [
+    RaisedButton(
+        onPressed: () => queueAction(NewCardAction.selectLocal()),
+        child: Text('Local card')),
+    RaisedButton(
+        onPressed: () => queueAction(NewCardAction.selectRemote()),
+        child: Text('Remote card')),
+  ]));
 
   return frame(
       title: Text('New card'),
@@ -71,22 +90,86 @@ Widget _renderNewCard(NewCardView newCardView, BuiltMap<NodeName, NodeState> nod
       backAction: () => queueAction(NewCardAction.back()));
 }
 
-Widget _renderCardSettings(CardSettingsView cardSettingsView, BuiltMap<NodeName, NodeState> nodesStates,
+Widget _renderNewCardLocal(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(NewCardAction) queueAction) {
+  // Saves current node name:
+  String _nodeName = '';
+
+  // TODO: Add the ability to pick name:
+  final body = Center(
+      child: Row(children: [
+    Spacer(flex: 1),
+    Expanded(
+        flex: 4,
+        child: Column(children: [
+          Expanded(
+              flex: 1,
+              child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                Text('Name:'),
+                Expanded(
+                    child: TextField(
+                        onChanged: (newNodeName) => _nodeName = newNodeName)),
+              ])),
+          Spacer(flex: 2),
+          Expanded(
+              flex: 1,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    RaisedButton(
+                        onPressed: () => queueAction(
+                            NewCardAction.newCardLocal(NodeName(_nodeName))),
+                        child: Text('Ok')),
+                    RaisedButton(
+                        onPressed: () => queueAction(NewCardAction.back()),
+                        child: Text('Cancel')),
+                  ])),
+        ])),
+    Spacer(flex: 1),
+  ]));
+
+  return frame(
+      title: Text('New local card'),
+      body: body,
+      backAction: () => queueAction(NewCardAction.back()));
+}
+
+Widget _renderNewCardRemote(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(NewCardAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderNewRemoteName(
+    RemoteCardFile remoteCardFile,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(NewCardAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderCardSettings(
+    CardSettingsView cardSettingsView,
+    BuiltMap<NodeName, NodeState> nodesStates,
     Function(CardSettingsAction) queueAction) {
   throw UnimplementedError();
 }
 
-Widget _renderSelectCardAddRelay(RelayAddress relayAddress, BuiltMap<NodeName, NodeState> nodesStates,
+Widget _renderSelectCardAddRelay(
+    RelayAddress relayAddress,
+    BuiltMap<NodeName, NodeState> nodesStates,
     Function(SettingsAction) queueAction) {
   throw UnimplementedError();
 }
 
-Widget _renderSelectCardAddIndex(IndexServerFile indexServerFile, BuiltMap<NodeName, NodeState> nodesStates,
+Widget _renderSelectCardAddIndex(
+    IndexServerFile indexServerFile,
+    BuiltMap<NodeName, NodeState> nodesStates,
     Function(SettingsAction) queueAction) {
   throw UnimplementedError();
 }
 
-Widget _renderSelectCardAddFriend(FriendFile friendFile, BuiltMap<NodeName, NodeState> nodesStates,
+Widget _renderSelectCardAddFriend(
+    FriendFile friendFile,
+    BuiltMap<NodeName, NodeState> nodesStates,
     Function(SettingsAction) queueAction) {
   throw UnimplementedError();
 }
