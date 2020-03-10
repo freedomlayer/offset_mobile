1e600c709af3960e93230f27bf5f46bb007774ce
real <real@freedomlayer.org>
2020-03-10 13:41:43 +0200

    render: frame: Added backAction

--- lib/render/frame.dart
+++ lib/render/frame.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'consts.dart';
 
-Widget frame({@required Widget title, @required Widget body}) {
+Widget frame(
+    {@required Widget title, @required Widget body, Function() backAction}) {
+  Widget appTitle;
+  if (backAction != null) {
+    // We provide a back arrow to the title bar:
+    appTitle = Row(
+        children: [BackButton(onPressed: () => backAction()), title]);
+  } else {
+    // No back arrow:
+    appTitle = title;
+  }
+
   return MaterialApp(
     title: APP_TITLE,
     home: Scaffold(
       appBar: AppBar(
-        title: title,
+        title: appTitle,
       ),
       body: body,
     ),
