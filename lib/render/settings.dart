e51e733f66bd7312570a3bfe6a56866f1678f5e3
real <real@freedomlayer.org>
2020-03-10 15:30:11 +0200

    render: Initial work on settings view

--- lib/render/settings.dart
+++ lib/render/settings.dart
@@ -5,9 +5,33 @@ import '../protocol/protocol.dart';
 import '../state/state.dart';
 import '../actions/actions.dart';
 
-Widget renderSettings(SettingsView settingsView,
-    BuiltMap<NodeName, NodeState> nodesStates, Function(SettingsAction) queueAction) {
-  throw UnimplementedError();
+import 'frame.dart';
+
+Widget renderSettings(
+    SettingsView settingsView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(SettingsAction) queueAction) {
+
+  final children = <Widget>[];
+  for (final entry in nodesStates.entries) {
+    final nodeName = entry.key;
+    // final nodeState = entry.value;
+    final cardEntry = ListTile(key: Key(nodeName.inner), title: Text('$nodeName'));
+    children.add(cardEntry);
+  }
+
+  final listView = ListView(
+      padding: const EdgeInsets.all(8),
+      children: children);
+
+  final newCardButton = FloatingActionButton.extended(
+      onPressed: () => queueAction(SettingsAction.selectNewCard()),
+      label: Text('New card'),
+      icon: Icon(Icons.add));
+
+  return frame(
+      title: Text('Settings'),
+      body: listView,
+      backAction: () => queueAction(SettingsAction.back()),
+      floatingActionButton: newCardButton);
 }
-
-
