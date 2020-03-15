4fb9f7d3d0474a3670d7f15e49fb9f27f9e07ec2
real <real@freedomlayer.org>
2020-03-15 18:20:47 +0200

    render: renderFriendSettings() outline

--- lib/render/settings/friend.dart
+++ lib/render/settings/friend.dart
@@ -11,7 +11,6 @@ import '../../actions/actions.dart';
 // import '../utils/file_picker.dart';
 // import '../utils/qr_scan.dart';
 
-
 // import '../../logger.dart';
 
 // import '../frame.dart';
@@ -19,6 +18,46 @@ import '../../actions/actions.dart';
 Widget renderFriendSettings(
     NodeName nodeName,
     FriendSettingsView friendSettingsView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(FriendSettingsAction) queueAction) {
+
+  final friendPublicKey = friendSettingsView.friendPublicKey;
+  return friendSettingsView.inner.match(
+      home: () => _renderFriendHome(nodeName, friendPublicKey, nodesStates, queueAction),
+      resolve: () => _renderResolve(nodeName, friendPublicKey, nodesStates, queueAction),
+      currencySettings: (currency) => _renderCurrencySettings(nodeName, friendPublicKey, currency, nodesStates, queueAction),
+      newCurrency: () => _renderNewCurrency(nodeName, friendPublicKey, nodesStates, queueAction));
+}
+
+
+Widget _renderFriendHome(
+    NodeName nodeName,
+    PublicKey friendPublicKey,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(FriendSettingsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderResolve(
+    NodeName nodeName,
+    PublicKey friendPublicKey,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(FriendSettingsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderCurrencySettings(
+    NodeName nodeName,
+    PublicKey friendPublicKey,
+    Currency currency,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(FriendSettingsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderNewCurrency(
+    NodeName nodeName,
+    PublicKey friendPublicKey,
     BuiltMap<NodeName, NodeState> nodesStates,
     Function(FriendSettingsAction) queueAction) {
   throw UnimplementedError();
