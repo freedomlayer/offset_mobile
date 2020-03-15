3cb8c98cde549f52052164fc2c4bdfa400a3e081
real <real@freedomlayer.org>
2020-03-15 17:04:08 +0200

    render: utils: Added share_file

---
+++ lib/render/utils/share_file.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_extend/share_extend.dart';
+
+import '../../protocol/serialize.dart';
+
+import 'package:path/path.dart' as path;
+
+const String SHARE_DIR = 'share';
+
+// TODO: Is there any issue if this function is called twice
+// with the same shareFilename? Possibly randomize filename or directory name
+// to avoid issues?
+
+/// Share a file with another application
+Future<void> shareFile<T>(T msg, String shareFilename) async {
+  // Get app's directory:
+  // Directory appDir = await getApplicationDocumentsDirectory();
+  Directory tempDir = await getTemporaryDirectory();
+
+  // Make sure SHARE_DIR directory exists:
+  final shareDirPath = path.join(tempDir.path, SHARE_DIR);
+  print('shareDirPath = $shareDirPath');
+  await Directory(shareDirPath).create();
+
+  // Serialize message into a string:
+  final data = serializeMsg<T>(msg);
+
+  // Write to file:
+  final shareFilePath = path.join(shareDirPath, shareFilename);
+  File shareFile = File(shareFilePath);
+  shareFile.writeAsStringSync(data);
+
+  // Share (local) file:
+  ShareExtend.share(shareFile.path, "file");
+}
