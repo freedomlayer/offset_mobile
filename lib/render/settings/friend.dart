4f3009bb2339e2e5771fe3a8c73298f911d13ef8
real <real@freedomlayer.org>
2020-03-15 18:57:49 +0200

    render: Some work on _renderFriendHome(). Not done yet

--- lib/render/settings/friend.dart
+++ lib/render/settings/friend.dart
@@ -12,7 +12,7 @@ import '../../actions/actions.dart';
 
 // import '../../logger.dart';
 
-// import '../frame.dart';
+import '../frame.dart';
 
 Widget renderFriendSettings(
     NodeName nodeName,
@@ -31,32 +31,54 @@ Widget renderFriendSettings(
           nodeName, friendPublicKey, friendReport, queueAction));
 }
 
-Widget _renderFriendHome(
-    NodeName nodeName,
-    PublicKey friendPublicKey,
-    FriendReport friendReport,
-    Function(FriendSettingsAction) queueAction) {
+Widget _renderFriendHome(NodeName nodeName, PublicKey friendPublicKey,
+    FriendReport friendReport, Function(FriendSettingsAction) queueAction) {
+  // TODO:
+  final listView = ListView(children: []);
 
-  throw UnimplementedError();
+  final body = Center(
+      child: Column(children: [
+    Expanded(flex: 1, child: ListTile(title: Text(friendReport.name))),
+    Expanded(
+        flex: 1,
+        child: SwitchListTile(
+            value: friendReport.status.isEnabled,
+            title: Text("Enable"),
+            onChanged: (bool newValue) {
+              if (newValue == true) {
+                queueAction(FriendSettingsAction.enableFriend());
+              } else {
+                queueAction(FriendSettingsAction.disableFriend());
+              }
+            })),
 
-  /*
-  final body = Center(child: Column(children: [
-    Expanded(flex: 1, child: ListTile(title: )),
-    Expanded(flex: 10, listView),
+    // TODO: Possibly move "unfriend" to a place where it is less likely to be clicked
+    // on accidentally? Maybe some drawer that opens when three dots are clicked?
+    Expanded(
+        flex: 1,
+        child: ListTile(
+            title: Text('Unfriend'),
+            onTap: () => queueAction(FriendSettingsAction.removeFriend()))),
+    Expanded(flex: 1, child: ListTile(title: Text('Currencies'))),
+    Expanded(flex: 10, child: listView),
   ]));
+
+  // TODO: Possibly move "New Currency" to a place where it is less likely to be clicked
+  // on accidentally? Maybe some drawer that opens when three dots are clicked?
+  final newCurrencyButton = FloatingActionButton.extended(
+      onPressed: () => queueAction(FriendSettingsAction.selectNewCurrency()),
+      label: Text('New Currency'),
+      icon: Icon(Icons.add));
 
   return frame(
       title: Text('${nodeName.inner}: Friend Settings'),
       body: body,
-      backAction: () => queueAction(RelaysSettingsAction.back()));
-  */
+      backAction: () => queueAction(FriendSettingsAction.back()),
+      floatingActionButton: friendReport.status.isEnabled ? newCurrencyButton : null);
 }
 
-Widget _renderResolve(
-    NodeName nodeName,
-    PublicKey friendPublicKey,
-    FriendReport friendReport,
-    Function(FriendSettingsAction) queueAction) {
+Widget _renderResolve(NodeName nodeName, PublicKey friendPublicKey,
+    FriendReport friendReport, Function(FriendSettingsAction) queueAction) {
   throw UnimplementedError();
 }
 
@@ -69,10 +91,7 @@ Widget _renderCurrencySettings(
   throw UnimplementedError();
 }
 
-Widget _renderNewCurrency(
-    NodeName nodeName,
-    PublicKey friendPublicKey,
-    FriendReport friendReport,
-    Function(FriendSettingsAction) queueAction) {
+Widget _renderNewCurrency(NodeName nodeName, PublicKey friendPublicKey,
+    FriendReport friendReport, Function(FriendSettingsAction) queueAction) {
   throw UnimplementedError();
 }
