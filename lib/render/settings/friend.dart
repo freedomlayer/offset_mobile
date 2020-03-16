56e62902838e04fd6d5587d71c888a9072a2927a
real <real@freedomlayer.org>
2020-03-16 13:51:26 +0200

    render: _renderFriendHome() + _renderNewCurrency()

--- lib/render/settings/friend.dart
+++ lib/render/settings/friend.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../protocol/protocol.dart';
 // import '../../protocol/file.dart';
@@ -67,8 +68,8 @@ Widget _renderFriendHome(NodeName nodeName, PublicKey friendPublicKey,
       }
 
       final double ratePercent = (configReport.rate.mul / (1 << 32)) * 100;
-      final subtitle = Text('limit: ${configReport.remoteMaxDebt}, ' +
-          'rate: ${ratePercent.toStringAsFixed(2)}% + configReport.rate.add');
+      final subtitle = Text('limit: ${configReport.remoteMaxDebt.inner}' +
+          '\nrate: ${ratePercent.toStringAsFixed(2)}% + ${configReport.rate.add}');
 
       children.add(ListTile(
         key: Key(currency.inner),
@@ -142,7 +143,137 @@ Widget _renderCurrencySettings(
   throw UnimplementedError();
 }
 
+String _currencyNameValidator(String currencyName) {
+  if (currencyName.isEmpty) {
+    return 'Currency name can not be empty!';
+  }
+
+  if (currencyName.length > 16) {
+    return 'Currency name too long!';
+  }
+
+  if (currencyName.length > 16) {
+    return 'Currency name too long!';
+  }
+
+  final validChars = RegExp(r'^[A-Z]+$');
+  if (!validChars.hasMatch(currencyName)) {
+    return 'Currency name must only contain capital letters';
+  }
+  return null;
+}
+
+String _creditLimitValidator(String creditLimitString) {
+  if (creditLimitString.isEmpty) {
+    return 'credit limit can not be empty!';
+  }
+
+  final BigInt creditLimit = BigInt.tryParse(creditLimitString);
+  if (creditLimit == null) {
+    return 'Invalid credit limit value';
+  }
+
+  if (creditLimit < BigInt.from(0)) {
+    return 'Credit limit must be non negative!';
+  }
+
+  return null;
+}
+
 Widget _renderNewCurrency(NodeName nodeName, PublicKey friendPublicKey,
     FriendReport friendReport, Function(FriendSettingsAction) queueAction) {
-  throw UnimplementedError();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  Currency _currency;
+  U128 _creditLimit;
+
+  final _submitForm = () {
+    final FormState form = _formKey.currentState;
+
+    if (!form.validate()) {
+      // Form is not valid
+    } else {
+      // Save form fields:
+      form.save(); 
+      final rate = Rate((b) => b
+        ..mul = 0
+        ..add = 0);
+
+      queueAction(
+          FriendSettingsAction.newCurrency(_currency, _creditLimit, rate));
+    }
+  };
+
+
+  final body =
+      StatefulBuilder(builder: (BuildContext context, StateSetter setState) {
+    final form = Form(
+        key: _formKey,
+        autovalidate: true,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.attach_money),
+                hintText: 'Currency name',
+                labelText: 'Name',
+              ),
+              inputFormatters: [LengthLimitingTextInputFormatter(16)],
+              validator: _currencyNameValidator,
+              keyboardType: TextInputType.text,
+              onSaved: (currencyName) => _currency = Currency(currencyName),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.cake),
+                hintText: 'Maximum amount friend can owe me',
+                labelText: 'Credit limit',
+              ),
+              validator: _creditLimitValidator,
+              keyboardType: TextInputType.number,
+              onSaved: (creditLimitString) =>
+                  _creditLimit = U128(BigInt.tryParse(creditLimitString)),
+            ),
+            Container(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Spacer(flex: 1),
+                      Expanded(
+                          flex: 2,
+                          child: RaisedButton(
+                            child: const Text('Ok'),
+                            onPressed: _submitForm,
+                          )),
+                      Spacer(flex: 1),
+                      Expanded(
+                          flex: 2,
+                          child: RaisedButton(
+                            child: const Text('Cancel'),
+                            onPressed: () =>
+                                queueAction(FriendSettingsAction.back()),
+                          )),
+                      Spacer(flex: 1),
+                    ])),
+          ],
+        ));
+
+    return SafeArea(
+        top: false,
+        bottom: false,
+        child: Center(
+            child: Column(children: [
+          Spacer(flex: 1),
+          Expanded(
+              flex: 1, child: Text('${nodeName.inner} / ${friendReport.name}')),
+          Expanded(flex: 16, child: form),
+        ])));
+  });
+
+  return frame(
+      title: Text('New Currency'),
+      body: body,
+      backAction: () => queueAction(FriendSettingsAction.back()));
 }
