4964535ce84825e758030e92313021feca1888d1
real <real@freedomlayer.org>
2020-03-17 18:15:36 +0200

    render: Implemented _renderSelectCard()

--- lib/render/balances.dart
+++ lib/render/balances.dart
@@ -5,8 +5,47 @@ import '../protocol/protocol.dart';
 import '../state/state.dart';
 import '../actions/actions.dart';
 
+import 'frame.dart';
+
 Widget renderBalances(
     BalancesView balancesView,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(BalancesAction) queueAction) {
+  return balancesView.match(
+      selectCard: () => _renderSelectCard(nodesStates, queueAction),
+      cardBalances: (nodeName) =>
+          _renderCardBalances(nodeName, nodesStates, queueAction));
+}
+
+Widget _renderSelectCard(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(BalancesAction) queueAction) {
+  final children = <Widget>[];
+  for (final entry in nodesStates.entries) {
+    final nodeName = entry.key;
+    final nodeState = entry.value;
+    final cardEntry = ListTile(
+      key: Key(nodeName.inner),
+      title: Text('${nodeName.inner}'),
+      enabled: nodeState.inner.isOpen,
+      leading: Icon(Icons.credit_card),
+      onTap: nodeState.inner.isOpen
+          ? () => queueAction(BalancesAction.selectCard(nodeName))
+          : null,
+    );
+
+    children.add(cardEntry);
+  }
+
+  final listView = ListView(padding: EdgeInsets.all(8), children: children);
+
+  return frame(
+      title: Text('Balances'),
+      body: listView,
+      backAction: () => queueAction(BalancesAction.back()));
+}
+
+Widget _renderCardBalances(
+    NodeName nodeName,
     BuiltMap<NodeName, NodeState> nodesStates,
     Function(BalancesAction) queueAction) {
   throw UnimplementedError();
