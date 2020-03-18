e9b90debf717ef080b0f24dfa7e36f15bd9c8637
real <real@freedomlayer.org>
2020-03-18 12:56:15 +0200

    render: renderInTransactions() outline

--- lib/render/in_transactions.dart
+++ lib/render/in_transactions.dart
@@ -9,5 +9,41 @@ Widget renderInTransactions(
     InTransactionsView inTransactionsView,
     BuiltMap<NodeName, NodeState> nodesStates,
     Function(InTransactionsAction) queueAction) {
+  return inTransactionsView.match(
+      home: () => _renderHome(nodesStates, queueAction),
+      transaction: (nodeName, invoiceId) =>
+          _renderTransaction(nodesStates, queueAction),
+      sendInvoice: (nodeName, invoiceId) =>
+          _renderSendInvoice(nodesStates, queueAction),
+      collected: (nodeName, invoiceId) =>
+          _renderCollected(nodesStates, queueAction),
+      selectCardApplyCommit: (commit) =>
+          _renderSelectCardApplyCommit(commit, nodesStates, queueAction));
+}
+
+Widget _renderHome(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(InTransactionsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderTransaction(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(InTransactionsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderSendInvoice(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(InTransactionsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderCollected(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(InTransactionsAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderSelectCardApplyCommit(
+    Commit commit,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(InTransactionsAction) queueAction) {
   throw UnimplementedError();
 }
