52157fae8c603edd239960db450f54bae70adaaa
real <real@freedomlayer.org>
2020-03-18 15:44:10 +0200

    render: renderSell() outline

--- lib/render/sell.dart
+++ lib/render/sell.dart
@@ -7,5 +7,30 @@ import '../actions/actions.dart';
 
 Widget renderSell(SellView sellView, BuiltMap<NodeName, NodeState> nodesStates,
     Function(SellAction) queueAction) {
+  return sellView.match(
+      selectCard: () => _renderSelectCard(nodesStates, queueAction),
+      invoiceDetails: (nodeName) =>
+          _renderInvoiceDetails(nodeName, nodesStates, queueAction),
+      sendInvoice: (nodeName, invoiceId) =>
+          _renderSendInvoice(nodeName, invoiceId, nodesStates, queueAction));
+}
+
+Widget _renderSelectCard(BuiltMap<NodeName, NodeState> nodesStates,
+    Function(SellAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderInvoiceDetails(
+    NodeName nodeName,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(SellAction) queueAction) {
+  throw UnimplementedError();
+}
+
+Widget _renderSendInvoice(
+    NodeName nodeName,
+    InvoiceId invoiceId,
+    BuiltMap<NodeName, NodeState> nodesStates,
+    Function(SellAction) queueAction) {
   throw UnimplementedError();
 }
