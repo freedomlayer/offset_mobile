a0678282b61ec772d94455b5e7a6ed46ac5a98c1
real <real@freedomlayer.org>
2020-04-12 18:57:04 +0300

    render: friends: Updated font weight

--- lib/render/settings/friends.dart
+++ lib/render/settings/friends.dart
@@ -85,8 +85,8 @@ Widget _renderHome(NodeName nodeName, NodeState nodeState,
   final listView = children.isNotEmpty
       ? ListView(children: children, padding: EdgeInsets.all(8))
       : Center(
-          child: Text('No friends configured',
-              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)));
+          child: Text('No friends configured'));
+              
 
   final body = Column(children: [
     Container(
